453228440adf847a58f46b4b1a454e289129325f
mehtaharsh929 <mehtaharsh929@gmail.com>
2020-02-14 11:57:43 +0530

    second commit

--- 2-arrow-function.js
+++ 2-arrow-function.js
@@ -11,9 +11,18 @@
 
 const event = {
   name: "Birthday Party",
- guest:
+  guestList: ["raj", "ram", "ravi"],
+
   printGuestList() {
+    // const that = this;
+
     console.log("Guest list " + this.name);
+
+    // this.guestList.forEach(function(guest) {
+    // Arrow function does bind this function
+    this.guestList.forEach(guest => {
+      console.log(guest + "is attending" + this.name);
+    });
   }
 };
 event.printGuestList();
