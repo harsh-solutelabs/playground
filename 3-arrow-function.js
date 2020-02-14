9f4b3394c901bbf5ced28ea2e9f4c395ad29f0cc
mehtaharsh929 <mehtaharsh929@gmail.com>
2020-02-14 13:04:43 +0530

    third commit

---
+++ 3-arrow-function.js
@@ -0,0 +1,38 @@
+//
+// Goal: Create method to get incomplete tasks
+//
+// 1. Define getTasksToDo method
+// 2. Use filter to to return just the incompleted tasks (arrow function)
+// 3. Test your work by running the script
+
+const tasks = {
+  tasks: [
+    {
+      text: "Grocery shopping",
+      completed: true
+    },
+    {
+      text: "Clean yard",
+      completed: false
+    },
+    {
+      text: "Film course",
+      completed: false
+    }
+  ],
+  //   getTasksToDo() {
+  //     const newTask = this.tasks.filter(incomplete => {
+  //       return incomplete.completed === false;
+  //     });
+  //     return newTask;
+  //   }
+
+  getTasksToDo() {
+    const newTask = this.tasks.filter(
+      incomplete => incomplete.completed === false
+    );
+    return newTask;
+  }
+};
+
+console.log(tasks.getTasksToDo());
