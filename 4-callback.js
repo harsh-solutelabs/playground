90293b696566556e21a2dcc8c4cf8d643606dc8a
mehtaharsh929 <mehtaharsh929@gmail.com>
2020-02-17 11:42:30 +0530

    Callbacck Function

---
+++ 4-callback.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  console.log("Two seconds are up");
+}, 2000);
+
+const names = ["rabv", "raj", "param"];
+
+const four = names.filter(name => {
+  return name.length <= 4;
+});
+
+const geo = (location, callBack) => {
+  setTimeout(() => {
+    const data = {
+      latitude: 0,
+      logitude: 0
+    };
+    callBack(data);
+  }, 2000);
+};
+
+geo("phili", data => {
+  console.log(data);
+});
+
+const add = (val1, val2, callBack) => {
+  setTimeout(() => {
+    const data = val1 + val2;
+    callBack(data);
+  }, 2000);
+};
+
+add(1, 4, sum => {
+  console.log(sum);
+});
---
+++ wea.js
@@ -0,0 +1,9 @@
+console.log("Starting");
+setTimeout(() => {
+  console.log("first Timer");
+}, 5000);
+
+setTimeout(() => {
+  console.log("2 Second Timer");
+}, 3000);
+console.log("Ending");
